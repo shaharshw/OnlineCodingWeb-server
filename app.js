460ea3aaa62478cd7dabf99d8e587a877ab9fdd7
shaharshw <shwartzshahar23@gmail.com>
2024-05-12 10:25:53 +0300

    fixing seesions and remove from package.json

--- app.js
+++ app.js
@@ -2,27 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const { connectToDb } = require('./config/dbConnection.js');
-const { initSocket, io } = require('./socket.js');
+const { initSocket } = require('./socket.js');
 const codeBlockRoutes = require("./routes/codeBlockRoutes");
-const session = require('express-session');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(session({
-  secret: 'your-secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Note: set secure to true if you are using https
-}));
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    req.session.user = { role: 'student' }; // Initialize user object in session with default role
-  }
-  next();
-});
 app.use(errorHandler);
 
 // Connect to MongoDB
