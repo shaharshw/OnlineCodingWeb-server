a471a028d254b4b536a76ce899dea18e8998a39d
Shahar Shwartz <99726451+shaharshw@users.noreply.github.com>
2024-07-23 18:22:48 +0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectToDb = async () => {
-    mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(mongodb+srv://shaharshwartz: TelAviv1997@cluster0.n5lp6lf.mongodb.net/coding_app?retryWrites=true&w=majority&appName=Cluster0 , { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Connected to MongoDB'))
         .catch(err => console.error('Error connecting to MongoDB:', err));
 }
 
 
 module.exports = { connectToDb };
+
+
+//process.env.CONNECTION_STRING
